ae0a25efbe9fcbc14d88bf26a3338c830e7c2de0
adbak <adbak.dev@gmail.com>
2025-02-15 19:24:15 +0100

    Initial Commit

---
+++ scrum/scrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumApp.swift
+//  scrum
+//
+//  Created by Adnan Bakaev  on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct scrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
